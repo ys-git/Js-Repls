6b9a048e246cc7a7696f6f2d9d26d13d85b0c38b
Yogendra Singh <32202840+ys-git@users.noreply.github.com>
2021-12-29 21:48:54 +0000

    updated code

--- array_pairs.js
+++ array_pairs.js
@@ -1,13 +1,10 @@
-const arr = [2,4,5,6,1,7,3];
+const arr = [2, 4, 5, 6, 1, 7, 3];
 var num;
 
-for(n in arr)
-{
+for (n in arr) {
   num = arr[n];
-  for(k in arr)
-  {
-    if(k != arr.length-1 && num != arr[k])
-    {
+  for (k in arr) {
+    if (k != arr.length - 1 && num != arr[k]) {
       console.log(num + "," + arr[k++]);
     }
   }

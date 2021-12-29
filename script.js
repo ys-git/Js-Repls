69d38a42e5ba0c6828aafd908dced46a78f87c2c
Yogendra Singh <32202840+ys-git@users.noreply.github.com>
2021-12-29 13:49:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const arr = [2,4,5,6,1,7,3];
+var num;
+
+for(n in arr)
+{
+  num = arr[n];
+  for(k in arr)
+  {
+    if(k != arr.length-1 && num != arr[k])
+    {
+      console.log(num + "," + arr[++k]);
+    }
+  }
+
+  console.log("---------------------------------");
+}

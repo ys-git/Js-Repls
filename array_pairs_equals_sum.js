c9b0f3659de68b243a9f52712c64be7b23af9d51
Yogendra Singh <32202840+ys-git@users.noreply.github.com>
2022-01-02 11:05:02 +0000

    Better approach for checkPairsEqualsSum with time complexity O(n)

--- array_pairs_equals_sum.js
+++ array_pairs_equals_sum.js
@@ -4,7 +4,7 @@
 
 //----Naive approach---//
 
-let arr1 = [1,4,7,5,9];
+let arr1 = [1,4,5,7,9];
 let sum = 8;
 
 function checkPairsEqualsSum()
@@ -25,4 +25,37 @@ function checkPairsEqualsSum()
   return false;
 }
 
-console.log(checkPairsEqualsSum());
+//console.log(checkPairsEqualsSum());
+
+//Time Complexity - O(n^2)
+
+//-------------------------------------------------------------
+
+//Better approach
+
+function checkPairsEqualsSum1()
+{
+  for(var i = 0, j = arr1.length-1 ; i < j ; )
+  {
+    let s = arr1[i] + arr1[j];
+    if(s === sum)
+    {
+      return true;
+    }
+
+    if(s > sum)
+    {
+      j--;
+    }
+    else
+    {
+      i++;
+    }
+  }
+
+  return false;
+}
+
+//console.log(checkPairsEqualsSum1());
+
+//Time Complexity - O(n)
